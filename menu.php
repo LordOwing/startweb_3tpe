acee2b5d08856062b6b65f456ac5a158e7f05a2c
LordOwing <michallolo12345@gmail.com>
2025-01-21 15:14:11 +0100

    Cos

--- menu.php
+++ menu.php
@@ -9,3 +9,14 @@ $menu = array (
 		)
 	)
 );
+$menu = array (
+	'Dashboard' => array('page' => 'index', 'name' => 'Pulpit'),
+	 'News' => array('page' => 'news', 'name' => 'News',
+	 'actions' => array (
+	 0 => array ('action' => 'add', 'title' => 
+	'Dodaj aktualność')
+	)
+	),
+	'Users' => array('page' => 'users', 'name' => 
+	'Użytkownicy'));
+	
